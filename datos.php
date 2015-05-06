636482c90afff8438fe6532c1a93e0c1d1f47511
sminutoli <sminutoli.it@gmail.com>
2015-05-06 19:00:33 -0300

    Updated

---
+++ datos.php
@@ -0,0 +1,38 @@
+<?php
+
+  //esto después va a venir de una base de datos, ahora me sirve así!
+
+  $productosDB = [
+    [
+      'id'=>1,
+      'nombre'=>'Producto numero 1',
+      'precio'=>450.34,
+      'imagen'=>'http://lorempixel.com/100/70/fashion/1'
+    ],
+    [
+          'id'=>2,
+          'nombre'=>'Otro producto',
+          'precio'=>1000.99,
+          'imagen'=>'http://lorempixel.com/100/70/fashion/2'
+    ],
+    [
+          'id'=>3,
+          'nombre'=>'Más productos',
+          'precio'=>33.90,
+          'imagen'=>'http://lorempixel.com/100/70/fashion/3'
+    ],
+    [
+          'id'=>4,
+          'nombre'=>'El mejor de todos los productos',
+          'precio'=>150,
+          'imagen'=>'http://lorempixel.com/100/70/fashion/4'
+    ],
+    [
+          'id'=>5,
+          'nombre'=>'Este producto tiene un nombre muy largo, habría que cortarlo',
+          'precio'=>566,
+          'imagen'=>'http://lorempixel.com/100/70/fashion/5'
+    ]
+  ];
+
+?>
